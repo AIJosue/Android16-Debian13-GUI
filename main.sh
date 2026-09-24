44c9905db275b0a2f3e6958167304742bda52d6e
Josue <118592169+AIJosue@users.noreply.github.com>
2026-09-23 20:23:36 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# 1. Update package lists 123
+# 1. Update package lists 
 sudo apt update
 
+# 2. install packages 
 sudo apt install -y cockpit-* incus* podman* distrobox
-
-# 2. Install KDE Plasma Desktop and Xorg
-# Using --no-install-recommends keeps the installation lighter
-#sudo apt install -y --no-install-recommends #xserver-xorg kde-plasma-desktop sddm
-
-# 3. Install XRDP for remote access
-sudo apt install -y xrdp
-
-# 4. Install KWin X11 (Window Manager)
-#sudo apt install -y kwin-x11
-
-# 5. Set the system to boot into graphical mode by default
-#sudo systemctl set-default graphical.target
-
-echo "Setup complete. Please set a password for your user if you haven't already."
