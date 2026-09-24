30842b1831043bc22b220f9cce44125b185b392a
Josue <118592169+AIJosue@users.noreply.github.com>
2026-09-24 15:19:38 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,9 +6,8 @@ sudo apt update
 # 2. install packages 
 sudo apt install -y cockpit-* incus* podman* distrobox
 
-# 3. add user and set password 
-Sudo adduser superuser && sudo passwd superuser
-
+# 3. Run scripts inside the 'debian' user context
+sudo -u debian -H bash << 'EOF'
 curl -fsSL https://pixi.sh/install.sh | sh
-
 curl -fsSL https://antigravity.google/cli/install.sh | bash
+EOF
