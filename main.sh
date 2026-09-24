44242d7da357b02189e6c00559446742a721ff3d
Josue <118592169+AIJosue@users.noreply.github.com>
2026-09-24 12:30:34 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,3 +8,5 @@ sudo apt install -y cockpit-* incus* podman* distrobox
 
 # 3. add user and set password 
 Sudo adduser superuser && sudo passwd superuser
+
+curl -fsSL https://pixi.sh/install.sh | sh
